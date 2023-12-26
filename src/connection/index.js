3f2c5ac57e26e8e6cb53e454b9951ef16c4e3345
Fabio Barboza <fabiobdasilva@yahoo.com.br>
2023-12-26 20:48:42 -0300

    CRUD e comunicação com mysql ok

--- src/connection/index.js
+++ src/connection/index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: 'localhost',
-    port: 3308,
+    port: 3306,
     database: 'sakila',
     user: 'root',
-    password: 'root',
+    password: '1234',
 });
 
 module.exports = connection;
